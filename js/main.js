74e29b9cdbd424c1b3fe30879f0893a0fa6ff7d0
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-05-30 14:21:13 +0700

    split css files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const createBook = (event) => {
   event.preventDefault();
-  document.querySelectorAll();
+  alert('f');
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  const submitForm = document.getElementById('book_form');
+  submitForm.addEventListener('submit', createBook);
+});
